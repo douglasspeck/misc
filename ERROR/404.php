dc58d57500be123f3f2a53b16dacbce926e6def8
douglasspeck <speck.empresarial@gmail.com>
2022-03-28 02:21:49 -0300

    Added custom 404 page

--- ERROR/404.php
+++ ERROR/404.php
@@ -10,11 +10,12 @@
     <main>
         <header>
             <h1>
-                <span class="subject">Erro 404 – Página não encontrada</span>
+                <span class="number">404</span>
+                <span class="name">Página não encontrada</span>
             </h1>
         </header>
         <section>
-            <p>Você tentou acessar um link que não existe ou não está disponível no momento.</p>
+            <p><strong>Oops!</strong> Você tentou acessar um link que não existe ou não está disponível no momento.</p>
             <p>Mas não se preocupe, você pode voltar para a <a href="../">Página Inicial</a> a qualquer momento.</p>
         </section>
     </main>
