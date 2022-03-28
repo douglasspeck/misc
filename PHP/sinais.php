7b9dcd117dc2c032addef0eb463b4ae95f1a2b70
douglasspeck <speck.empresarial@gmail.com>
2022-03-28 03:35:55 -0300

    Added sinais.php

---
+++ PHP/sinais.php
@@ -0,0 +1,7 @@
+<?php
+
+    for ($i = 0; $i < count($sinais); $i++) {
+        echo '<li><a class="" href="sinais/' . $sinais[$i] . '.php" target="sinais">' . $sinais[$i] . '</a></li>';
+    };
+
+?>
