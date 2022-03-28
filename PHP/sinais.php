7a550c74350fae5dec2a08427066abc20b1d9be3
douglasspeck <speck.empresarial@gmail.com>
2022-03-28 04:30:40 -0300

    EL213 – Aula 2

--- PHP/sinais.php
+++ PHP/sinais.php
@@ -1,7 +1,7 @@
 <?php
 
     for ($i = 0; $i < count($sinais); $i++) {
-        echo '<li><a class="" href="sinais/' . $sinais[$i] . '.php" target="sinais">' . $sinais[$i] . '</a></li>';
+        echo '<li><a class="" href="sinais?s=' . $sinais[$i] . '" target="sinais">' . $sinais[$i] . '</a></li>';
     };
 
 ?>
