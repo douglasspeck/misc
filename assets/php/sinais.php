a62ff35e5b4d53341749580005e79b31bb4357e0
douglasspeck <speck.empresarial@gmail.com>
2022-03-28 14:56:17 -0300

    Added paragraph list style

--- assets/php/sinais.php
+++ assets/php/sinais.php
@@ -1,7 +1,15 @@
 <?php
 
-    for ($i = 0; $i < count($sinais); $i++) {
-        echo '<li><a class="" href="sinais?s=' . $sinais[$i] . '" target="sinais">' . $sinais[$i] . '</a></li>';
-    };
+    if ($sign_list_type == 'paragraph') {
+        echo '<p><a class="" href="sinais?s=' . $sinais[0] . '" target="sinais">' . $sinais[0] . '</a>';
+        for ($i = 1; $i < count($sinais); $i++) {
+            echo ', <a class="" href="sinais?s=' . $sinais[$i] . '" target="sinais">' . $sinais[$i] . '</a>';
+        };
+        echo '</p>';
+    } else {
+        for ($i = 0; $i < count($sinais); $i++) {
+            echo '<li><a class="" href="sinais?s=' . $sinais[$i] . '" target="sinais">' . $sinais[$i] . '</a></li>';
+        };
+    }
 
 ?>
