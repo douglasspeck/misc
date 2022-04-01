976ed24649ca746a08fa18f788d85f8a00c1cf79
douglasspeck <speck.empresarial@gmail.com>
2022-04-01 11:27:53 -0300

    EL683 Aula 3

--- EL683/index.php
+++ EL683/index.php
@@ -11,8 +11,8 @@
     <main>
         <header>
             <h1>
-                <span class="tag"></span>
-                <span class="subject"></span>
+                <span class="tag">EL683</span>
+                <span class="subject">Escola e Cultura</span>
             </h1>
             <details class="header-details">
                 <summary>Professor Carlos Eduardo Albuquerque Miranda</summary>
@@ -64,6 +64,90 @@
                         <li>Web Art e Metaverso</li>
                         <li>Aculturação</li>
                     </ul>
+                </section>
+            </section>
+        </section>
+        <section>
+            <h2>Segunda aula</h2>
+            <section>
+                <h3>Anotações Gerais da Aula</h3>
+                <ul>
+                    <li>O curso de pedagogia é o <strong>único</strong> da Unicamp que não</li>
+                    <li>Desligar o celular no sábado e só ligar no domingo à noite.
+                        <ul>
+                            <li><q>Gozado... há dez anos atrás isso não era necessário.</q></li>
+                            <li><q>Na mesma época que o celular se torna nosso vício, a desigualdade aumenta no mundo.</q>
+                                <ul>
+                                    <li>Não é uma relação de causa e efeito, mas há uma relação.</li>
+                                </ul>
+                            </li>
+                            <li><q>Com quem eu to competindo? Com a Rede Globo? Com o TikTok? Que sentido tem em competir com isso?</q></li>
+                        </ul>
+                    </li>
+                    <li>Educação não se reduz à psicologia da aprendizagem.</li>
+                    <li><strong>Recomendação de leitura:</strong> <a href="http://www2.fct.unesp.br/docentes/geo/bernardo/BIBLIOGRAFIA%20DISCIPLINAS%20GRADUACAO/PENSAMENTO%20GEOGR%C1FICO%202017/3-Geografia(YvesLacoste).pdf" title="Link para o PDF" target="_blank">A Geografia: isso serve, em primeiro lugar, para fazer a guerra</a></li>
+                    <li><q>Se bem que eu acho que a disciplina não é um grande valor pro exército, pelo que a gente tem visto.</q></li>
+                </ul>
+            </section>
+            <section>
+                <h3>Dar a ver, dar a pensar: contra o domínio do automático</h3>
+                <p><a href="../assets/docs/Dar-a-ver-dar-a-pensar.pdf" title="Artigo" target="_blank">Referência Bibliográfica</a></p>
+                <blockquote>
+                    <p>Passamos o dia a olhar, mas seremos capazes de ver algo? Que relação existe entre ver e pensar? E em que sentido a percepção é um problema político?</p>
+                    <cite>FERNÁNDEZ-SAVATER, Amador</cite>
+                </blockquote>
+                <p>O pensar está associado não ao conhecimento, mas à mudança de percepção.</p>
+                <p>É o convite à mudança que provoca o "estalo" inicial do pensamento. O questionamento, a indignação.</p>
+                <blockquote>
+                    <p>Olhava, mas não via nada. Nada singular, que me afectasse.</p>
+                    <cite>FERNÁNDEZ-SAVATER, Amador</cite>
+                </blockquote>
+                <p>A esse convite, à imagem que nos faz ver, damos o nome de <dfn>Imagem Fecunda</dfn>.</p>
+                <section>
+                    <h4>Estereótipos</h4>
+                    <p>O estereótipo é uma caixinha fechada. Uma função aplicada repetitivamente sobre as coisas, de maneira automática e acrítica.</p>
+                    <p>Podem ter origens ideológicas, midiáticas, políticas... enfim, surge a partir de um pensamento estático, convicto e infundamentado. <q>Julgamo-nos muito singulares, mas somos feitos em série.</q> Uma receita de bolo que não é questionada.</p>
+                    <p>Essa visão fechada descarta tudo aquilo que não se encaixa nos estereótipos como mero ruído. Isto é, não apenas quer expandir-se, mas eliminar tudo aquilo que não constitui-lhe ou volta ao próprio.</p>
+                </section>
+                <p>O autor se percebe cego pelos códigos que aprendera ao longo de sua caminhada.</p>
+                <section>
+                    <h4>Parar o mundo</h4>
+                    <p>Mesmo os estudos das ciências sociais são elementos que não condizem diretamente com a realidade. As teorias filosófico-políticas são importantes, mas fundamental é a escuta.</p>
+                    <blockquote>
+                        <p>Pôr um pouco entre parênteses as teorias e pensar a partir de detalhes: essa foi a minha maneira particular de parar o mundo para ver. Um modo de entrar em contacto, deixar-se tocar e afectar pelo que acontecia.</p>
+                        <cite>FERNÁNDEZ-SAVATER, Amador</cite>
+                    </blockquote>
+                </section>
+                <p>De maneira geral, achei o livro bem racionalista, uma vez que supervaloriza aquilo que é "eterno, fixo e imutável" enquanto despreza a experiência sensível.</p>
+                <p>Dentro do âmbito pedagógico, a questão dos "estereótipos" é vista no método ensinado ao alunos para resolução de problemas. Tradicionalmente, lhes é dado uma "lente", isto é, um conjunto de ferramentas para resolver qualquer situação.</p>
+            </section>
+            <section>
+                <h3>E-ducando o Olhar: a necessidade de uma pedagogia pobre</h3>
+                <p><a href="../assets/docs/E-ducando-o-Olhar.pdf" title="Artigo" target="_blank">Referência Bibliográfica</a></p>
+                <p>O autor propõe dois sentidos diferentes para a <strong>educação do olhar:</strong> no sentido de ensinar (<i lang="la">educare</i>), revelar a verdade, despertar; e no ensino (<i lang="la">e-ducere</i>) de olhar pra fora, prestar atenção.</p>
+                <p>O autor compara o caminhar com um sobrevoo. Caminhar exige abandonar o olhar já visto, mudar a perspectiva.</p>
+                <blockquote>
+                    <p>Quando você chega no meio do caminho, você não tem outra escolha. Ou você vai ou você volta. [...] São esses os comandos da ideia do caminhar.</p>
+                </blockquote>
+                <p>O autor faz uma forte crítica à noção de "fim da linha", de uma terra prometida, tanto na perspectiva religiosa quanto marxista (teleológica).</p>
+                <blockquote>
+                    <p>Teorias tem que vir todas. [...] Mas não se aposta que a aplicação da teoria é a solução dos problemas.</p>
+                </blockquote>
+                <ul>
+                    <li>Quanto maior a crise, mais o capital se concentra.
+                        <ul>
+                            <li><q>O capital não tem nenhum compromisso com a democracia.</q></li>
+                        </ul>
+                    </li>
+                    <li>Sucesso escolar ≠ Ascenção Social
+                        <ul>
+                            <li><q>Educar pro futuro me parece uma furada. A gente tem que educar pro presente.</q></li>
+                        </ul>
+                    </li>
+                </ul>
+                <section>
+                    <h4>Como deixar o olhar vago e guiar-se pela experiência em um sistema que nos exige o oposto?</h4>
+                    <p>É importante fazer um acompanhamento por relato subjetivo (apesar de a subjetividade ser construída socialmente), um estudo do meio.</p>
                 </section>
             </section>
         </section>
