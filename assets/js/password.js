8d8347b6fee8dd1807268cd143c8f5a0af6a582b
douglasspeck <speck.empresarial@gmail.com>
2022-04-01 17:07:50 -0300

    Added a spare time to login system

--- assets/js/password.js
+++ assets/js/password.js
@@ -1,4 +1,26 @@
-document.onload(askPassword());
+window.onload = function() {
+    login();
+}
+
+function login() {
+
+    const MAX_TIME_IN_MINUTES = 10;
+
+    // minutes to milisseconds
+    const MAX_TIME = MAX_TIME_IN_MINUTES * 60 * 1000;
+
+    let isLogged = localStorage.getItem('isLogged');
+    let lastLogin = parseInt(localStorage.getItem('lastLogin'));
+
+    let now = new Date().getTime();
+    let loggedTime = now - lastLogin;
+
+    if (isLogged == "true" && loggedTime < MAX_TIME) {
+        console.log('User is already logged');
+        localStorage.setItem('lastLogin', now);
+    }
+    else { askPassword() }
+}
 
 function askPassword() {
 
@@ -6,6 +28,12 @@ function askPassword() {
 
     let input = prompt('Senha de Acesso:', '');
 
-    if (input === PASS) { return }
+    if (input === PASS) {
+        let now = new Date();
+
+        localStorage.setItem('isLogged', 'true');
+        localStorage.setItem('lastLogin', now.getTime());
+        console.log('User just logged')
+    }
     else { window.location.replace('../') }
 }
