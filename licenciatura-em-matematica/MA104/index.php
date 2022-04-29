ec540141e09d1b3b636017437fffd6a9e9947695
douglasspeck <speck.empresarial@gmail.com>
2022-04-29 00:26:05 -0300

    Palestra do Lino

--- licenciatura-em-matematica/MA104/index.php
+++ licenciatura-em-matematica/MA104/index.php
@@ -6,7 +6,7 @@
         $keywords = '';
         $resources = [];
         include '../../assets/php/head.php';
-    ?>
+    ?> 
 <body>
     <?php
         include '../assets/php/pages.php';
@@ -57,7 +57,64 @@
                 <li><q>Tem muita coisa que é importante, mas não é útil</q> <cite>Firer</cite></li>
                 <li><q>O único jeito de formar 20% de bons alunos é ensinar 100%</q> <cite>Firer</cite></li>
                 <li><q>Ninguém vai à academia para ser o melhor levantador de peso</q> <cite>Desconhecido</cite></li>
+                <li><q>Deixa eu desligar aqui</q> <cite>Lino de Macedo</cite></li>
             </ul>
+        </section>
+        <section>
+            <h2>Abstração Matemática e Jogos</h2>
+            <section>
+                <h3>O Palestrante</h3>
+                <p><strong>Nome:</strong> Lino de Macedo</p>
+                <p><strong>Telefone:</strong> (11) 99101-6396</p>
+                <p><strong>E-mail:</strong> limacedo@me.com</p>
+                <ul>
+                    <p><strong>Atribuições:</strong></p>
+                    <li>Professor Emérito do Instituto de Psicologia (<abbr title="Universidade de São Paulo">USP</abbr>)</li>
+                    <li>Membro da Academia Paulista de Psicologia</li>
+                    <li>Assessor do Instituto Pensi (<abbr title="Fundação José Luiz Egydio Setúbal">FJLES</abbr>)</li>
+                    <li>Membro da Cátedra Educação Básica do Instituto de Estudos Avançados (<abbr title="Universidade de São Paulo">USP</abbr>)</li>
+                </ul>
+            </section>
+            <section>
+                <h3>Introdução</h3>
+                <ul>
+                    <li>Epistemologia Genética
+                        <ul>
+                            <li><q>Genético no sentido de Gene</q> <span class="arr-r"></span> Pensando em crianças</li>
+                            <li>Como aplicar o conhecimento científico entre crianças?</li>
+                        </ul>
+                    </li>
+                    <li>PAG definiu duas disciplinas fundamentais para o ensino científico de crianças: Física, que trata da contemplação da realidade, e a Matemática, que compreende o pensamento racional (formal).</li>
+                    <li>PAG admite que a gente aprende com a experiência
+                        <ul>
+                            <li>Experiência física/empírica</li>
+                            <li>Experiência mental
+                                <ul>
+                                    <li><q>Os números não existem. Onde está o número?</q></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <h3>Por que e como aprender matemática?</h3>
+                <p>Pensar bem implica "operações matemáticas", ainda que não sejam formalizadas.</p>
+                <ul>
+                    <p>Como aprender matemática?</p>
+                    <li>Via ensino?</li>
+                    <li>Via desenvolvimento de competências e habilidades?</li>
+                    <li>De forma individual?</li>
+                    <li>De forma coletiva?</li>
+                </ul>
+            </section>
+            <section>
+                <h3>Kenken</h3>
+                <p><dfn>Kenken</dfn> é um jogo criado por Kentsua Miyamoto baseado em Sudoku.</p>
+                <p>É constituído por um tabuleiro quadrado de \(n \times n\) casas que, assim como no Sudoku, devemos preencher com números de 1 a n, sem repetir os números em uma mesma linha ou coluna.</p>
+                <p>Seu diferencial é a presença de "blocos" com operações que devem ser realizadas entre os números do bloco de forma a resultar em um valor superscrito.</p>
+                <p>Habilidades/Mediadores Internos</p>
+            </section>
         </section>
         </main>
     <?php include '../../assets/php/scripts.php' ?>
