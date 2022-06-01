0a3500b574364ee11386f5dff231d2c1304fb831
douglasspeck <speck.empresarial@gmail.com>
2022-06-01 18:40:36 -0300

    Email template

--- licenciatura-em-matematica/sepromat/email.php
+++ licenciatura-em-matematica/sepromat/email.php
@@ -3,6 +3,6 @@
 const name = "professor Eduardo Wagner";
 const message = "Gostaríamos de convidá-lo para ministrar uma palestra com foco no ensino de matemática ou até mesmo contar relatos de sua vivência como professor, acreditamos que o senhor tem muito para nos agregar e nos ensinar dando dicas e conselhos sobre o ensino. Ficaríamos muito lisonjeados com a sua presença e participação já que muitos de nós te conhecemos pelos livros da coleção do professor de matemática e pelo PAPMEM sendo grandes admiradores da sua didática e do seu trabalho.";
 
-include "template-sepromat.html";
+include "template.html";
 
 ?>
