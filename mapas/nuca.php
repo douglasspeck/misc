f5ab6eebe08c3461c0377e06d3cdeeae02d69992
douglasspeck <speck.empresarial@gmail.com>
2023-11-07 22:32:33 -0300

    Map updates

---
+++ mapas/nuca.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <?php
+        $title = 'Mapas de Douglas Speck';
+        $description = 'Registros pessoais de minhas viagens e informações aleatórias sobre os lugares em que já estive';
+        $keywords = '';
+        $resources = [];
+        include '../assets/php/head.php';
+    ?>
+<body>
+    <main>
+        <h1>Núcleo Campinas</h1>
+        <?php echo(file_get_contents("../assets/img/sp.svg")) ?>
+    </main>
+</body>
+</html>
