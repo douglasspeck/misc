d60a144284e36160499e119c1acdd782a1007187
douglasspeck <speck.empresarial@gmail.com>
2023-11-18 01:19:59 -0300

    PC Backup

--- mapas/nuca.php
+++ mapas/nuca.php
@@ -10,6 +10,40 @@
 <body>
     <main>
         <h1>Núcleo Campinas</h1>
+        <section id="map-menu">
+            <input type="radio" name="menu" id="input-IES">
+            <label for="input-IES">
+                <p>IES</p>
+                <ul>
+                    <li>Não possui IES</li>
+                    <li>1-3 IES</li>
+                    <li>4-5 IES</li>
+                    <li>6+ IES</li>
+                </ul>
+            </label>
+            <input type="radio" name="menu" id="input-agentes">
+            <label for="input-agentes">
+                <p>Agentes Parceiros</p>
+                <ul>
+                    <li>Não possui agentes parceiros</li>
+                    <li>1-2 agentes parceiros</li>
+                    <li>3-4 IES</li>
+                    <li>5+ IES</li>
+                </ul>
+            </label>
+            <input type="radio" name="menu" id="input-ejs-x-ies">
+            <label for="input-ejs-x-ies">
+                <p>IES x EJs</p>
+                <ul>
+                    <li>Nenhuma das IES possui EJs</li>
+                    <li>0.0 - 0.2</li>
+                    <li>0.2 - 0.4</li>
+                    <li>0.4 - 0.6</li>
+                    <li>0.6 - 0.8</li>
+                    <li>0.8 - 1.0</li>
+                </ul>
+            </label>
+        </section>
         <?php echo(file_get_contents("../assets/img/nuca.svg")) ?>
     </main>
 </body>
